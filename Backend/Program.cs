f491b1a28b10bf7db88129a1a1c6e9511093672b
borntobehor <hleanghor3@gmail.com>
2026-08-10 00:33:45 +0700

    cors origin

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -14,6 +14,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend", policy =>
     {
         policy
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://borntobehor.github.io/"
+            )
             .AllowAnyOrigin()
             .AllowAnyMethod()
             .AllowAnyHeader();
